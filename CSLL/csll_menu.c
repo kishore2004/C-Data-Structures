be6aa57222ce343acb8f3df92e98d6a2dd7a713d
Kishore <kvg.kishore2004@gmail.com>
2026-09-04 21:01:50 +0530

    Added insert beginning and insert end functions

--- CSLL/csll_menu.c
+++ CSLL/csll_menu.c
@@ -10,6 +10,10 @@ Node *create();
 
 void display(Node *head);
 
+Node *insertb(Node *head);
+
+Node *inserte(Node *head);
+
 Node *freeList(Node *head);
 
 int main()
@@ -22,6 +26,8 @@ int main()
         printf("=====CSLL Menu=====\n");
         printf("1. Create Circular Singly Linked List\n");
         printf("2. Display List\n");
+        printf("3. Insert at Beginning\n");
+        printf("4. Insert at End\n");
         printf("14. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &ch);
@@ -33,6 +39,12 @@ int main()
                 break;
             case 2:
                 display(head);
+                break;
+            case 3:
+                head=insertb(head);
+                break;
+            case 4:
+                head=inserte(head);
                 break;
             case 14:
                 running=0;
@@ -53,6 +65,12 @@ Node *create()
     int n;
     printf("Enter the number of nodes: ");
     scanf("%d", &n);
+
+    if(n<=0)
+    {
+        printf("Invalid number of nodes\n");
+        return NULL;
+    }
 
     Node *head=NULL;
     Node *temp=NULL;
@@ -106,8 +124,77 @@ void display(Node *head)
     }
 }
 
+Node *insertb(Node *head)
+{
+    Node *last=NULL;
+    Node *newNode=malloc(sizeof(*newNode));
+    if(newNode==NULL)
+    {
+        printf("Memory allocation failed\n");
+        return head;
+    }
+    printf("Enter the data: ");
+    scanf("%d", &(newNode->data));
+    newNode->next=NULL;
+    
+    if(head==NULL)
+    {
+        head=newNode;
+        return head;
+    }
+    else
+    {
+        Node *temp=head;
+        while(temp->next!=head)
+        {
+            temp=temp->next;
+        }
+        last=temp;
+        newNode->next=head;
+        head=newNode;
+        last->next=head;
+        return head;
+    }
+}
+
+Node *inserte(Node *head)
+{
+    Node *last=NULL;
+    Node *temp=head;
+    Node *newNode=NULL;
+    newNode=malloc(sizeof(*newNode));
+    if(newNode==NULL)
+    {
+        printf("Memory allocation failed\n");
+        return head;
+    }
+    printf("Enter the data: ");
+    scanf("%d", &(newNode->data));
+    newNode->next=NULL; 
+    if(head==NULL)
+    {
+        head=newNode;
+        return head;
+    }
+    else
+    {
+        temp=head;
+        while(temp->next!=head)
+        {
+            temp=temp->next;
+        }
+        temp->next=newNode;
+        newNode->next=head;
+        return head;
+    }
+}
+
 Node *freeList(Node *head)
 {
+    if(head==NULL)
+    {
+        return NULL;
+    }
     Node *temp=head;
     while(temp->next!=head)
     {
